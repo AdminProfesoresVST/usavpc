accf571232cea21fca4eb7083a48ab67b045fa27
appsdev <appsdev@villacampast.com>
2025-12-07 21:38:27 -0400

    fix: ensure bottom navbar visible on tablet devices

--- components/layout/MobileNav.tsx
+++ components/layout/MobileNav.tsx
@@ -31,7 +31,7 @@ export function MobileNav() {
     ];
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 pb-safe md:hidden">
+        <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 pb-safe xl:hidden">
             <div className="flex justify-around items-center h-16">
                 {navItems.map((item) => {
                     const Icon = item.icon;
